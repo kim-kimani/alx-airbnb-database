a279c3eba0d599157464d191175e302e5fcbba20
km-isource <kimsykimanimburu@gmail.com>
2025-05-16 08:50:04 +0300

    Add EXPLAIN ANALYZE test queries to database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -12,3 +12,14 @@ CREATE INDEX IF NOT EXISTS idx_properties_host_id ON Properties(host_id);
 
 -- Index for check_in_date in Bookings (used in availability reports)
 CREATE INDEX IF NOT EXISTS idx_bookings_check_in_date ON Bookings(check_in_date);
+
+
+- Performance Test Queries using EXPLAIN ANALYZE (required for checker)
+
+-- Query 1: Find bookings by guest
+EXPLAIN ANALYZE
+SELECT * FROM Bookings WHERE guest_id = 'user_001';
+
+-- Query 2: Find bookings by property
+EXPLAIN ANALYZE
+SELECT * FROM Bookings WHERE property_id = 'property_101';
